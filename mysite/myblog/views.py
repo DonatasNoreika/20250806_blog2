91a637435b875600217acf5e2dd5b3259208d2f0
DonatasNoreika <donoras@gmail.com>
2025-08-06 21:47:29 +0300

    Pridėtas login/logout

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
     return render(request, template_name="index.html")
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = "signup.html"
+    success_url = reverse_lazy("login")
