98642ee8182435ad8701d8fd275aa8694a80f520
DonatasNoreika <donoras@gmail.com>
2025-08-06 21:58:13 +0300

    Prisijungusiam vartotojui rodomi jo įrašai

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("posts/", views.PostListView.as_view(), name='posts'),
+    path("userposts/", views.UserPostListView.as_view(), name='userposts'),
     path("signup/", views.SignUpView.as_view(), name="signup"),
 ]
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,18 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     template_name = "signup.html"
     success_url = reverse_lazy("login")
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = "posts.html"
+    context_object_name = "posts"
+
+class UserPostListView(generic.ListView):
+    model = Post
+    template_name = "posts.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
+
+
